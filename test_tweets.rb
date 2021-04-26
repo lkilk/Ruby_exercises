ad9ac941cb8f4fce083dec6ab9fd3b910043d493
Liam Kilkenny <81643363+lkilk@users.noreply.github.com>
2021-04-26 17:09:25 +0100

    Create test_tweets.rb

---
+++ test_tweets.rb
@@ -0,0 +1,16 @@
+test_tweets = [
+  "This president sucks!",
+  "I hate this Blank House!",
+  "I can't believe we're living under such bad leadership. We were so foolish",
+  "President Presidentname is a danger to society. I hate that he's so bad – it sucks."
+  ]
+
+banned_phrases = ["sucks", "bad", "hate", "foolish", "danger to society"]
+
+test_tweets.each do |tweet|
+  banned_phrases.each do |banned|
+    tweet.gsub! banned, 'CENSORED'
+  end
+
+  puts tweet
+end
